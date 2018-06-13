4b54e5062787ae146a6fbd5492f1a52d06441658
Liberi <carlosliberi@gmail.com>
2018-06-13 09:31:00 -0300

    adding package json and @peresbruno fix

--- src/android/FileChooser.java
+++ src/android/FileChooser.java
@@ -3,6 +3,7 @@ package com.megster.cordova;
 import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Environment;
 import android.util.Log;
 
 import org.apache.cordova.CordovaArgs;
@@ -22,21 +23,41 @@ public class FileChooser extends CordovaPlugin {
     public boolean execute(String action, CordovaArgs args, CallbackContext callbackContext) throws JSONException {
 
         if (action.equals(ACTION_OPEN)) {
-            chooseFile(callbackContext);
+
+            String[] mimes = null;
+
+            if (args.getJSONArray(0) != null && args.getJSONArray(0).length() > 0) {
+                mimes = new String[args.getJSONArray(0).length()];
+                for (int i =0; i < args.getJSONArray(0).length(); i++) {
+                    mimes[i] = args.getJSONArray(0).getString(i);
+                }
+            }
+
+            chooseFile(callbackContext, mimes);
             return true;
         }
 
         return false;
     }
 
-    public void chooseFile(CallbackContext callbackContext) {
+    public void chooseFile(CallbackContext callbackContext, String[] mimes) {
 
         // type and title should be configurable
 
-        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        Intent intent;
+        if (mimes != null) {
+            intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+            intent.putExtra(Intent.EXTRA_MIME_TYPES, mimes);
+        }
+        else {
+            intent = new Intent(Intent.ACTION_GET_CONTENT);
+        }
+
+        intent.putExtra("android.content.extra.SHOW_ADVANCED", true);
         intent.setType("*/*");
         intent.addCategory(Intent.CATEGORY_OPENABLE);
         intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
+
 
         Intent chooser = Intent.createChooser(intent, "Select File");
         cordova.startActivityForResult(this, chooser, PICK_FILE_REQUEST);
